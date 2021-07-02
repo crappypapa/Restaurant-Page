daf23537b31309557ae783f6dadf9f56b398d1d2
crappypapa <devsolaoluwa@gmail.com>
2021-07-02 15:30:00 +0100

    Add webpack Config file

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const { mainModule } = require('process');
+
+module.exports = {
+	mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+	devServer: {
+		contentBase: './dist',
+	},
+};
