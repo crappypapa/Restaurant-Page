a93af943ebf08c90bcd29c4b875c4a27f890bf89
crappypapa <devsolaoluwa@gmail.com>
2021-07-03 08:50:22 +0100

    Create Navbar elements

---
+++ src/navbar.js
@@ -0,0 +1,46 @@
+export const navBar = () => {
+  const nav = document.createElement('nav');
+
+  const brand = document.createElement('a');
+  brand.classList.add('navbar-brand');
+  brand.textContent = 'Restaurant';
+
+  const navList = document.createElement('ul');
+  navList.classList.add('navbar-nav');
+
+  const navHome = document.createElement('li'); //creating the li tag
+  const home = document.createElement('a'); //creating the a tag
+  home.classList.add('nav-link'); // creating the class for a tag
+  home.textContent = 'HOME',  //creating the home text for the a tag
+  navHome.classList.add('nav-item'); // creating class for li tag
+  home.id = 'home'; //setting an id for the home menu
+  navHome.appendChild(home); //adding the a tag into the li tag
+
+  
+  const navMenu = document.createElement('li'); //creating the li tag
+  const menu = document.createElement('a'); //creating the a tag
+  menu.classList.add('nav-link'); // creating the class for a tag
+  menu.textContent = 'MENU',  //creating the home text for the a tag
+  navMenu.classList.add('nav-item'); // creating class for li tag
+  menu.id = 'menu'; //setting an id for the home menu
+  navMenu.appendChild(menu); //adding the a tag into the li tag
+
+  
+  const navContact = document.createElement('li'); //creating the li tag
+  const contact = document.createElement('a'); //creating the a tag
+  contact.classList.add('nav-link'); // creating the class for a tag
+  contact.textContent = 'CONTACT', //creating the home text for the a tag
+  navContact.classList.add('nav-item'); // creating class for li tag
+  contact.id = 'contact'; //setting an id for the home menu
+  navContact.appendChild(contact); //adding the a tag into the li tag
+
+  navList.appendChild(navHome);
+  navList.appendChild(navMenu);
+  navList.appendChild(navContact);
+
+  nav.appendChild(brand);
+  nav.appendChild(navList);
+
+  return nav;
+};
+
