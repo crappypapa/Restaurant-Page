192e5e2f45f6ea47d14257e3f5acd650e1eb9eda
crappypapa <devsolaoluwa@gmail.com>
2021-07-05 10:11:43 +0100

    Add other JS modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import { navBar } from './navbar';
+import { contact } from './contact';
+import { menu } from './menu';
+import { home } from './home';
+import './main.scss';
 
 const body = document.getElementsByTagName('body')[0];
 body.prepend(navBar());
+
+const contentDiv = document.getElementById('content');
+contentDiv.append(home());
+contentDiv.append(menu());
+contentDiv.append(contact());
+
