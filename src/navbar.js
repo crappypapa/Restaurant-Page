f49f5f5e48cbf65fb1bb6a38617497a5b13d4a58
crappypapa <devsolaoluwa@gmail.com>
2021-07-05 09:51:53 +0100

    Added bootstrap classers to elements

--- src/navbar.js
+++ src/navbar.js
@@ -1,17 +1,19 @@
 export const navBar = () => {
   const nav = document.createElement('nav');
+  nav.classList = 'navbar navbar-dark bg-dark'
 
   const brand = document.createElement('a');
-  brand.classList.add('navbar-brand');
+  brand.classList = 'navbar-brand px-3';
   brand.textContent = 'Restaurant';
 
   const navList = document.createElement('ul');
-  navList.classList.add('navbar-nav');
+  navList.classList = 'navbar-nav';
 
   const navHome = document.createElement('li'); //creating the li tag
   const home = document.createElement('a'); //creating the a tag
-  home.classList.add('nav-link'); // creating the class for a tag
-  home.textContent = 'HOME',  //creating the home text for the a tag
+  home.classList.add('nav-link');
+  home.href = '#home'; // creating the class for a tag
+  home.textContent = 'HOME';  //creating the home text for the a tag
   navHome.classList.add('nav-item'); // creating class for li tag
   home.id = 'home'; //setting an id for the home menu
   navHome.appendChild(home); //adding the a tag into the li tag
@@ -19,7 +21,8 @@ export const navBar = () => {
   
   const navMenu = document.createElement('li'); //creating the li tag
   const menu = document.createElement('a'); //creating the a tag
-  menu.classList.add('nav-link'); // creating the class for a tag
+  menu.classList.add('nav-link');
+  menu.href = '#menu'; // creating the class for a tag
   menu.textContent = 'MENU',  //creating the home text for the a tag
   navMenu.classList.add('nav-item'); // creating class for li tag
   menu.id = 'menu'; //setting an id for the home menu
@@ -28,7 +31,8 @@ export const navBar = () => {
   
   const navContact = document.createElement('li'); //creating the li tag
   const contact = document.createElement('a'); //creating the a tag
-  contact.classList.add('nav-link'); // creating the class for a tag
+  contact.classList.add('nav-link');
+  contact.href = '#contact'; // creating the class for a tag
   contact.textContent = 'CONTACT', //creating the home text for the a tag
   navContact.classList.add('nav-item'); // creating class for li tag
   contact.id = 'contact'; //setting an id for the home menu
