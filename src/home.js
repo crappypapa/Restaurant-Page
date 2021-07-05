62ffa001f0bb283220826d5b612ae21da945f7ef
crappypapa <devsolaoluwa@gmail.com>
2021-07-05 10:10:20 +0100

    Add the Home section

---
+++ src/home.js
@@ -0,0 +1,12 @@
+export const home = () => {
+  const landing = document.createElement('div');
+  landing.classList.add('bg-image');
+  landing.id = 'home';
+
+  const heading = document.createElement('h1');
+  heading.innerText = 'We speak the good food language';
+
+  landing.appendChild(heading);
+
+  return landing;
+};
